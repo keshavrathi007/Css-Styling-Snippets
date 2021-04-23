9b2ed56eba98ba9a7663a7a2403a17df0597e433
keshavrathi007 <45044122+keshavrathi007@users.noreply.github.com>
2021-04-24 01:32:54 +0530

    Add files via upload

---
+++ Line Through Effect - CSS and JavaScript/README.markdown
@@ -0,0 +1,5 @@
+# 
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/keshavrathi007/pen/ZELVZxP](https://codepen.io/keshavrathi007/pen/ZELVZxP).
+
+
---
+++ Line Through Effect - CSS and JavaScript/dist/script.js
@@ -0,0 +1,14 @@
+const text=document.getElementById('text');
+const textArr = text.innerText.split('');
+console.log(textArr);
+const newEl=document.createElement("h1");
+newEl.innerHTML=`
+${textArr.map(letter=>`<span style="${randomVisibility()}">${letter}</span>`
+).join('')}
+`;
+newEl.classList.add("overlay");
+document.body.appendChild(newEl);
+
+function randomVisibility(){
+  return Math.random() < 0.5 ? 'visibility:hidden' : 'visibility:visible';
+}
